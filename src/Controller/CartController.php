3c57c118e1f403969c290ea1f74f013d6aeb8c83
patrick <misterpat972@gmail.com>
2023-02-16 21:53:48 +0100

    update_21h

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CartController extends AbstractController
 {   
     #[Route('/mon-panier', name: 'cart')]
-    public function index(Request $request): Response
+    public function index(Request $request, ProductRepository $productRepository ): Response
     {
-       
-        dd($request->getSession()->get('cart'));
+        $cart = [];  
+        // récupération du panier
+        foreach ($request->getSession()->get('cart') as $id => $quantity) {
+            $cart[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+        // affichage du panier
 
-        return $this->render('cart/index.html.twig');
+
+        return $this->render('cart/panier.html.twig',[
+            'cart' => $cart
+        ]);
     }
     // ajout d'un produit dans le panier 
     #[Route('/mon-panier/add/{id}', name: 'add_cart')]
-        public function addCart(Cart $cart, $id, Request $request): Response
+        public function addCart(Cart $cart, $id): Response
     {
         $cart->add($id);
         return $this->redirectToRoute('cart');
