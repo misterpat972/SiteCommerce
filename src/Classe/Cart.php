881884c5a17b232bb3b50428a9dcd7ab9093f9ef
patrick <misterpat972@gmail.com>
2023-02-21 18:58:34 +0100

    update_21_02_2023

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,19 +2,40 @@
 
 namespace App\Classe;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
-
 
 
 class Cart 
 {
    private $requestStack;
+   private $productRepository;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, ProductRepository $productRepository)
     {
         $this->requestStack = $requestStack;
+        $this->productRepository = $productRepository;
     }
-
+    // voir la totalité du panier 
+    public function getFull()
+    {   // function get() récupère le panier dans la session
+        $cart = $this->get();
+        $fullCart = [];
+        foreach($cart as $id => $quantity){
+            $product_objet = $this->productRepository->find($id);
+            // si le produit n'existe pas on le supprime du panier
+            if(!$product_objet){
+                $this->removeProduct($id);
+                continue;
+            }
+            // si le produit existe on l'ajoute au panier
+            $fullCart[] = [
+                'product' => $product_objet,
+                'quantity' => $quantity
+            ];
+        }
+        return $fullCart;
+    }
     // ajout d'un produit dans le panier
     public function add($id)
     {
@@ -25,7 +46,21 @@ class Cart
         }else{
             $cart[$id] = 1;
         }
-        $session->set('cart', $cart);
+       return $session->set('cart', $cart);
+
+    }
+    // soustraction d'un produit dans le panier
+    public function sub($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        if($cart[$id] > 1) {
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+        
+       return $session->set('cart', $cart);
 
     }
     // récupération du panier
@@ -33,6 +68,16 @@ class Cart
     {
         $session = $this->requestStack->getSession();
         return $session->get('cart');
+    }
+    // suppression d'un produit dans le panier
+    public function removeProduct($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        if(!empty($cart[$id])){
+            unset($cart[$id]);
+        }
+      return  $session->set('cart', $cart);
     }
     // suppression du panier
     public function remove()
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -12,35 +12,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CartController extends AbstractController
 {   
     #[Route('/mon-panier', name: 'cart')]
-    public function index(Request $request, ProductRepository $productRepository ): Response
+    public function index(Cart $cart): Response
     {
-        $cart = [];  
-        // récupération du panier
-        foreach ($request->getSession()->get('cart') as $id => $quantity) {
-            $cart[] = [
-                'product' => $productRepository->find($id),
-                'quantity' => $quantity
-            ];
-        }
         // affichage du panier
-
-
         return $this->render('cart/panier.html.twig',[
-            'cart' => $cart
+            'cart' => $cart->getFull()
         ]);
     }
-    // ajout d'un produit dans le panier 
+    // ajout du nombre du produit dans le panier 
     #[Route('/mon-panier/add/{id}', name: 'add_cart')]
         public function addCart(Cart $cart, $id): Response
     {
         $cart->add($id);
         return $this->redirectToRoute('cart');
     }
+    // soustraction du nombre du produit dans le panier
+    #[Route('/mon-panier/sub/{id}', name: 'sub_product')]
+        public function sub(Cart $cart, $id): Response
+    {
+        $cart->sub($id);
+        return $this->redirectToRoute('cart');
+    }
     // suppression d'un produit dans le panier
+    #[Route('/mon-panier/remove/{id}', name: 'remove_product')]
+        public function removeProduct(Cart $cart, $id): Response
+    {
+        $cart->removeProduct($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    // suppression Total du panier
     #[Route('/mon-panier/remove', name: 'remove_cart')]
         public function removeCart(Cart $cart): Response
     {
         $cart->remove();
         return $this->redirectToRoute('products');
     }
+
 }
