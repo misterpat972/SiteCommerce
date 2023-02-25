af111dac9c6ef806fa837c59d52e209b4fddc06b
patrick <misterpat972@gmail.com>
2023-02-25 14:30:01 +0100

    update 25/02/2023

---
+++ src/Controller/AdressController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdressController extends AbstractController
+{
+    // function pour afficher les adresses du compte
+    #[Route('/compte/adresses', name: 'app_adress')]
+    public function index(AddressRepository $addressRepository): Response
+    {   
+       $addresse = $addressRepository->findBy(['user' => $this->getUser()]);
+        
+        return $this->render('account/address.html.twig',[                
+                'addresse' => $addresse,            
+        ]);
+        
+    }
+
+    // function pour ajouter une adresse au compte        
+    #[Route('/compte/ajouter-une-adresse', name: 'add_adress')]
+    public function addAdresse(Request $request, EntityManagerInterface $entityManager): Response
+    {   
+        // on crée une nouvelle adresse
+        $address = new Address();
+        // on crée le formulaire
+        $form = $this->createForm(AddressType::class, $address);
+        // on récupère les données du formulaire 
+        $form->handleRequest($request);
+        // si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on récupère les données du formulaire
+            $address = $form->getData();
+            // on lie l'adresse à l'utilisateur connecté
+            $address->setUser($this->getUser()); 
+            // on fige les données             
+            $entityManager->persist($address);
+            // on envoie les données en base de données
+            $entityManager->flush();
+            // on redirige vers la page des adresses du compte
+            return $this->redirectToRoute('app_adress');
+        }
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView(),
+        ]);        
+
+    }
+
+
+    // function pour modifier une adresse du compte 
+    #[Route('/compte/modifier-une-adresse/{id}', name: 'edit_adress')]
+    public function editAdresse(Request $request, EntityManagerInterface $entityManager, $id): Response
+    {
+       
+        $address = $entityManager->getRepository(Address::class)->find($id);
+        // on vérifie que l'adresse existe et qu'elle appartient à l'utilisateur connecté
+        if (!$address || $address->getUser() != $this->getUser()) {
+           $this->redirectToRoute('app_adress');            
+        }
+        // on crée le formulaire
+        $form = $this->createForm(AddressType::class, $address);
+        // on récupère les données du formulaire        
+        $form->handleRequest($request);
+        // si le formulaire est soumis et valide 
+        if ($form->isSubmitted() && $form->isValid()) {            
+            // on envoie les données en base de données
+            $entityManager->flush();
+            // on ajoute un message flash
+            $this->addFlash('success', 'Votre adresse a bien été modifiée');
+            // on redirige vers la page des adresses du compte
+            return $this->redirectToRoute('app_adress');
+        }
+        
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    // function pour supprimer une adresse du compte
+    #[route('/compte/supprimer-une-adresse/{id}', name: 'delete_adress')]
+    public function deleteAdresse(EntityManagerInterface $entityManager, $id): Response
+    {
+        // on supprime l'adresse
+        $address = $entityManager->getRepository(Address::class)->find($id);
+        // on vérifie que l'adresse existe et qu'elle appartient à l'utilisateur connecté
+        if (!$address || $address->getUser() != $this->getUser()) {
+            $this->redirectToRoute('app_adress');            
+        }
+        // on supprime l'adresse
+        $entityManager->remove($address);        
+        // on envoie les données en base de données
+        $entityManager->flush();
+        // on ajoute un message flash
+        $this->addFlash('success', 'Votre adresse a bien été supprimée');
+        // on redirige vers la page des adresses du compte
+        return $this->redirectToRoute('app_adress');
+    }
+}
---
+++ src/Form/AddressType.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            // champ pour le nom de l'adresse
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'adresse',
+                'attr' => [
+                    'placeholder' => 'Nommez votre adresse'
+                ]
+                ])
+                // champ pour le prénom
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votreprénom'
+                ]
+                ])
+                // champ pour le nom
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+                ])
+                // champ pour la société
+            ->add('company', TextType::class, [
+                'label' => 'Votre société',
+                'attr' => [
+                    'placeholder' => '(Facultatif), Entrez le nom de votre société'
+                ]
+                ])
+                // champ pour l'adresse
+            ->add('adress', TextType::class, [
+                'label' => 'Nom de l\'adresse',
+                'attr' => [
+                    'placeholder' => 'Ex: 8 rue de la paix....'
+                ]
+                ])
+                // champ pour le code postal
+            ->add('postal', TextType::class, [
+                'label' => 'Votre code postal',
+                'attr' => [
+                    'placeholder' => 'Entrez votre code postal'
+                ]
+                ])
+                // champ pour la ville
+            ->add('city', TextType::class, [
+                'label' => 'Votre ville',
+                'attr' => [
+                    'placeholder' => 'Entrez votre ville'
+                ]
+                ])
+                // champ pour le pays
+            ->add('country', CountryType::class, [
+                'label' => 'Votre pays',
+                'attr' => [
+                    'placeholder' => 'Entrez votre pays'
+                ]
+                ])
+                // champ pour le numéro de téléphone
+            ->add('phone', TelType::class, [
+                'label' => 'Votre téléphone',
+                'attr' => [
+                    'placeholder' => 'Entrez votre numéro de téléphone'
+                ]
+                ]) 
+            // bouton submit pour enregistrer l'adresse
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn-block btn-info'
+                ]
+                ]) 
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
